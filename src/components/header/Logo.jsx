3453c3ea21fafa4bd4b78b183d57c33c2726425d
chojaehyeok99 <choco5366@naver.com>
2024-06-29 17:32:05 +0900

    페이지 seo 작업

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -5,7 +5,7 @@ export const Logo = () => {
         <h1 className="header__logo">
             <a href='/'>
                 <em aria-hidden='true'></em>
-                <span>webs<br />youtube</span>
+                <span>JaeJae<br />youtube</span>
             </a>
         </h1>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Main from '../components/section/Main'
 
 const Home = () => {
     return (
-        <Main>
+        <Main title="JaeJae 유튜브" description="JaeJae 유튜버 사이트에 오신 것을 환영합니다.">
             Home
         </Main>
     )
